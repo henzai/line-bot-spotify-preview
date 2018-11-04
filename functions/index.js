d3e6b67040b5cb3d8633ab791cc27a75f7fb5ff8
henzai <ry0chord@gmail.com>
2018-11-05 06:13:11 +0900

    ２つ目の関数を追加

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,32 @@
 const functions = require('firebase-functions')
+const axios = require("axios")
 const line = require('@line/bot-sdk')
 const crypto = require('crypto')
-const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node')
+
+const BASE_URL = "https://asia-northeast1-line-bot-spotify-thirty-sec.cloudfunctions.net/"
 
 // Line の環境変数
 const CHANNEL_ACCESS_TOKEN = functions.config().line.bot.spotify.thirty.sec.access.token
 const CHANNEL_SECRET = functions.config().line.bot.spotify.thirty.sec.channel.secret
 
 // trackURIを引数にして、30秒試聴URLを返す.
-function getThirtySecondsURL(trackURI) {
-    var spotifyApi = new SpotifyWebApi();
-    spotifyApi.getTracks(trackId)
-    .then((data) => {
-        return data
-    }).catch( (err) => {
-        console.error(err)
-    })
-}
+exports.thirtySecondsURL = functions
+    .region('asia-northeast1')
+    .https.onRequest(async(request, response) => {
+        console.log(request.params)
+        let param = request.params[0].slice(1)
+
+        // const spotifyApi = new SpotifyWebApi();
+        // spotifyApi.getTracks(trackId)
+        // .then((data) => {
+        //     return data
+        // }).catch( (err) => {
+        //     console.error(err)
+        // })
+    
+        response.status(200).send(param)
+});
 
 // Signatureの検証
 function validateSignature(request) {
@@ -43,7 +53,7 @@ function newLineClient(){
 //
 exports.helloWorld = functions
     .region('asia-northeast1')
-    .https.onRequest((request, response) => {
+    .https.onRequest(async (request, response) => {
 
         // 署名の検証
         if (!validateSignature(request)){
@@ -54,7 +64,14 @@ exports.helloWorld = functions
         // テキストだったら
 
         // 規定のBaseURLだったら
+        try{
+            const url = await axios.get(BASE_URL +"thirtySecondsURL/6Dwtha2FtZFoMEBh5GR2sq")
+            console.log(url.data)
+        } catch (err) {
+            console.error(err)
+        }
 
+        
         // リクエストのログ
         console.log(request.body.events)
         const events = request.body.events
