8eda4d33cfafe39cf08024a0eb0f5d65ffdba465
henzai <ry0chord@gmail.com>
2018-11-08 03:47:45 +0900

    modify spotify.js

--- functions/spotify.js
+++ functions/spotify.js
@@ -1,9 +1,10 @@
+const functions = require('firebase-functions')
 const SpotifyWebApi = require('spotify-web-api-node')
 
 exports.newSpotifyClient = async function(){
     const spotifyApi = new SpotifyWebApi({
-        clientId: 'ec2c03f5d90d4509abc127fd2548e409',
-        clientSecret: 'bdacdf4c2fec489fafd4d900ba498615'
+        clientId: functions.config().spotify.client.id,
+        clientSecret: functions.config().spotify.client.secret
       })
       const token = await spotifyApi.clientCredentialsGrant()
       spotifyApi.setAccessToken(token.body['access_token'])
